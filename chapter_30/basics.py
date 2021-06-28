a7194439b30c9b66e63881c2566b443724a5a5d3
bimri <bimri@outlook.com>
2021-06-28 21:07:27 +0300

    basics to operator overloading == 'intercepting'

---
+++ chapter_30/basics.py
@@ -0,0 +1,14 @@
+"Operator Overloading"
+'''
+operator overloading” simply means intercepting built-in operations in a class’s
+methods
+    — Python automatically invokes your methods when instances of the class
+appear in built-in operations, and your method’s return value becomes the result of the
+corresponding operation.
+    • Operator overloading lets classes intercept normal Python operations.
+    • Classes can overload all Python expression operators.
+    • Classes can also overload built-in operations such as printing, function calls, attribute
+    access, etc.
+    • Overloading makes class instances act more like built-in types.
+    • Overloading is implemented by providing specially named methods in a class.
+'''
