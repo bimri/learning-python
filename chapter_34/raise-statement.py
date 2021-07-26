07bc95c240b31d9e00332aaeb7e4ad936772efa7
bimri <bimri@outlook.com>
2021-07-26 20:01:53 +0300

    raise statement

---
+++ chapter_34/raise-statement.py
@@ -0,0 +1,14 @@
+"The raise Statement"
+'''
+To trigger exceptions explicitly, you can code raise statements. Their general form is
+simple—a raise statement consists of the word raise, optionally followed by the class
+to be raised or an instance of it:
+'''
+raise instance              # raise instance of class           
+raise klass                 # Make and raise instance of class; makes an instance 
+raise                       # Re-raise last exception(most recent exception)
+
+
+# exceptions are always instances of classes 
+# first raise form here is the most common
+# raise is a statement, so it can be used anywhere
