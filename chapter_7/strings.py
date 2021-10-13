76263efc16fcab1cabf157b1ce4687f3056db8f9
bimri <50055672+bimri@users.noreply.github.com>
2021-10-13 23:26:54 +0300

    Update strings.py

--- chapter_7/strings.py
+++ chapter_7/strings.py
@@ -1,3 +1,3 @@
 # Implicit conctatenation
-title = "Meaning " 'of' " Life"         # adding commas will result in a tuiple & not a string
+title = "Meaning " 'of' " Life"         # adding commas will result in a tuple & not a string
 print(title)
