25ff6db567750602ce92c5480bc76aaf528f60b3
bimri <50055672+bimri@users.noreply.github.com>
2021-10-13 23:15:46 +0300

    Update escape_seqs.py

--- chapter_7/escape_seqs.py
+++ chapter_7/escape_seqs.py
@@ -1,5 +1,7 @@
-'''Escape sequences let us embed characters in strings 
-that cannot easily be typed on a keyboard'''
+'''
+Escape sequences let us embed characters in strings 
+that cannot easily be typed on a keyboard
+'''
 
 s = 'a\nb\tc'
 print(s)
