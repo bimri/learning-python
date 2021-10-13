094965601657f3cef0c3f752ed53d8fb1794ed7c
bimri <50055672+bimri@users.noreply.github.com>
2021-10-13 23:15:14 +0300

    Update dict_based_formatting.py

--- chapter_7/dict_based_formatting.py
+++ chapter_7/dict_based_formatting.py
@@ -2,16 +2,16 @@ print('%(qty)d more %(food)s' % {'qty': 1, 'food': 'spam'} )
 
 
 reply = '''                                         
-Greetings...
-Hello %(name)s!
-Your age is %(age)s
-'''
+        Greetings...
+        Hello %(name)s!
+        Your age is %(age)s
+        '''
 
 values = {'name': 'Bimri', 'age': 4000}             # Build up values to substitute
 print(reply % values)                               # Perform substitutions    
 
 
-food = 'spam'
+food = 'chapati'
 qty = 10
 
 print(vars())
