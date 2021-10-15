53e1c40dd2c15c91cffc2f5493b052ea72a30534
bimri <50055672+bimri@users.noreply.github.com>
2021-10-15 23:52:14 +0300

    Update nesting_in_dictionary.py

--- chapter_8/nesting_in_dictionary.py
+++ chapter_8/nesting_in_dictionary.py
@@ -8,8 +8,10 @@ print(rec)
 print(rec['name'])
 
 
-''' When nested Python's built-in data types allows
-    us to easily represent structured information.'''
+''' 
+When nested Python's built-in data types allows
+us to easily represent structured information.
+'''
 rec = {
     'name': 'Bimri',
     'jobs': ['developer', 'manager'],
@@ -24,26 +26,32 @@ print(rec['home']['zip'])
 print(rec)
 
 # Other ways to make dicts
-'''handy if you can spell out 
-    the entire dictionary ahead of time'''
+'''
+handy if you can spell out 
+the entire dictionary ahead of time
+'''
 
 {'name': 'Bimri', 'age': 40}                        # Traditional literal expression
 
-'''reate the dictionary one field at a 
-    time on the fly'''
+'''
+create the dictionary one field at a 
+time on the fly
+'''
 
 D = {}                                              # Assign by keys dynamically
 D['name'] = 'Bimri'
 D['age'] = 40
 
 
-'''requires all keys to be strings'''
+'requires all keys to be strings'
 
 dict(name='Bimri', age=40)                          # dict keyword argument form
 
 
-'''useful if you need to build up keys 
-and values as sequences at runtime'''
+'''
+useful if you need to build up keys 
+and values as sequences at runtime
+'''
 
 dict([('name', 'Bob'), ('age', 40)])                # dict key/value tuples form
 
